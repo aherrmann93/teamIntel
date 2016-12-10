826c632dc4a94c051629c3fda8a5b35d8ea8e93c
aherrmann93 <adam.herrmann@ucdconnect.ie>
2016-12-10 14:18:58 +0000

    Added variables, getters and setters for the sprite class

--- NearMiss/app/src/main/java/ie/ucd/nearmiss/Sprite.java
+++ NearMiss/app/src/main/java/ie/ucd/nearmiss/Sprite.java
@@ -1,9 +1,75 @@
 package ie.ucd.nearmiss;
 
+import android.graphics.Bitmap;
+
 /**
- * Created by adam on 10/12/2016.
+    Each game object will have the following attributes
  */
 
 public abstract class Sprite {
+    private int locx;       // location in x
+    private int locy;       // location in y
+    private int vecx;       // x vector
+    private int vecy;       // y vector
+    private int width;      // width of the object
+    private int height;     // height of the object
+    private Bitmap image;   // image of the object
 
+    // Getters and Setters for each of the above
+
+    public void setX(int x) {
+        this.locx = x;
+    }
+
+    public void setY(int y) {
+        this.locy = y;
+    }
+
+    public int getX() {
+        return locx;
+    }
+
+    public int getY() {
+        return locy;
+    }
+
+    public void setVecX(int x) {
+        this.vecx = x;
+    }
+
+    public void setVecY(int y) {
+        this.vecy = y;
+    }
+
+    public int getVecX() {
+        return vecx;
+    }
+
+    public int getVecY() {
+        return vecy;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
 }
