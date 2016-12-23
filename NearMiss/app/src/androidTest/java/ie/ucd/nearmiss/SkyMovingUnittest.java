85b5fbd79f57911717a715e6043863be785d7b1e
aherrmann93 <adam.herrmann@ucdconnect.ie>
2016-12-23 16:49:46 +0000

    Update Sky Moving Unit Test to Test Multiple times to check the sky is continuously moving

--- NearMiss/app/src/androidTest/java/ie/ucd/nearmiss/SkyMovingUnittest.java
+++ NearMiss/app/src/androidTest/java/ie/ucd/nearmiss/SkyMovingUnittest.java
@@ -7,10 +7,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertNotEquals;
 
 /**
- * This test checks that the sky moves at the specified rate of -5px/ms across the screen. This test is set up similarly to the GamePlayingDownUnitTest and checks that the sky has moved from the starting position and that its vector is set to the specified -5px/ms.
+ * This test checks that the sky moves at the specified rate of -5px/ms across the screen.
+ * This test is set up similarly to the GamePlayingDownUnitTest and checks that the sky has moved
+ * from the starting position and that its vector is set to the specified -5px/ms.
+ * The test checks multiple times (After 0.1, 1 and 2 seconds) to make sure the sky is continuously moving.
  *
  * This test verifies that the update method in the ControllerThread and Game classes are updating the Sky model correctly.
  *
@@ -37,6 +41,14 @@ public class SkyMovingUnitTest {
      * Level to pass into the game constructor
      */
     public int level = 1;
+    /**
+     * Sky position at Test 1
+     */
+    public int skyPos1;
+    /**
+     * Sky position at Test 2
+     */
+    public int skyPos2;
 
     /**
      * Set up the testing environment
@@ -52,19 +64,8 @@ public class SkyMovingUnitTest {
         game.createInitObjects();
         // Start game
         game.playing = true;
-        // Set plane going up
-        game.plane.goUp(true);
         // Wait for the game to update a few times
         Thread.sleep(100);
-    }
-
-    /**
-     * Check that the sky is moving
-     * @throws Exception
-     */
-    @Test
-    public void checkSkyHasMoved() throws Exception {
-        assertNotEquals("Check Sky has moved", 0, game.sky.getX(), 0);
     }
 
     /**
@@ -74,5 +75,34 @@ public class SkyMovingUnitTest {
     @Test
     public void checkSkyIsMovingatMinus5() throws Exception {
         assertEquals("Check sky is moving at -5px/ms",-5,game.sky.getVecX(),0);
+    }
+
+    /**
+     * Check that the sky has moved
+     * @throws Exception
+     */
+    @Test
+    public void checkSkyHasMoved() throws Exception {
+        assertNotEquals("Check Sky has moved", 0, skyPos1=game.sky.getX(), 0);
+    }
+
+    /**
+     * Check that the sky has moved further
+     * @throws Exception
+     */
+    @Test
+    public void checkSkyHasMoved1() throws Exception {
+        Thread.sleep(1000); // wait 1 second
+        assertTrue("Check Sky has moved", (skyPos2=game.sky.getX())<skyPos1);
+    }
+
+    /**
+     * Check that the sky has moved further again!
+     * @throws Exception
+     */
+    @Test
+    public void checkSkyHasMoved2() throws Exception {
+        Thread.sleep(2000); // wait 2 seconds
+        assertTrue("Check Sky has moved", game.sky.getX()<skyPos2);
     }
 }
